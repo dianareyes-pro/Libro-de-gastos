1869933d2cfb69cd62dec9533e1e2c48c1b61259
Diana Reyes <dianareyes100308gmail.com>
2026-09-10 22:02:32 -0500

    Agregar endpoint de prueba

---
+++ src/main/java/com/gestion/gastos/SaludoController.java
@@ -0,0 +1,13 @@
+package com.gestion.gastos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+
+    @GetMapping("/api/saludo")
+    public String saludo() {
+        return "Sistema de Control de Gastos funcionando correctamente";
+    }
+}
